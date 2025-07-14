0b28d06153530a9d55a0bf71e74f5dd176022e0a
Ulad Kasach <uladkasach@gmail.com>
2025-07-14 10:51:32 -0500

    fix(pkg): expose getGitRepoRoot procedure

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,2 +1,4 @@
 export { GitFile } from '../domain/GitFile';
 export { genArtifactGitFile } from '../logic/file/genArtifactGitFile';
+
+export { getGitRepoRoot } from '../logic/repo/getGitRepoRoot';
